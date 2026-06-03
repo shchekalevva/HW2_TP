11e4a858bd1a4279e8dc449ff78553516d94b239
Щекалева Александра <adschekaleva@edu.hse.ru>
2026-06-03 19:09:30 +0300

    Добавлены тесты для методов класса Recipe

--- test_recipes.py
+++ test_recipes.py
@@ -1,5 +1,5 @@
 import pytest
-from recipes import Ingredient
+from recipes import Ingredient, Recipe
 
 def test_ingredient_init():
     ing = Ingredient("Мука", 500.0, "г")
@@ -26,3 +26,44 @@ def test_ingredient_eq():
     assert ing1 == ing2
     assert ing1 != ing3
     assert ing1 != ing4
+
+def test_recipe_init():
+    recipe = Recipe("Пицца")
+    assert recipe.title == "Пицца"
+    assert recipe.ingredients == []
+
+def test_recipe_add_ingredient():
+    recipe = Recipe("Пицца")
+    ing1 = Ingredient("Мука", 500.0, "г")
+    ing2 = Ingredient("Соль", 1.0, "г")
+    ing3 = Ingredient("Мука", 200.0, "г")
+    
+    recipe.add_ingredient(ing1)
+    recipe.add_ingredient(ing2)
+    assert len(recipe.ingredients) == 2
+    
+    recipe.add_ingredient(ing3)
+    assert len(recipe.ingredients) == 2
+    assert recipe.ingredients[0].quantity == 700.0
+
+def test_recipe_scale():
+    recipe = Recipe("Пицца")
+    recipe.add_ingredient(Ingredient("Мука", 500.0, "г"))
+    recipe1 = recipe.scale(1.5)
+    
+    assert recipe1 is not recipe
+    assert recipe.ingredients[0].quantity == 500.0
+    assert recipe1.ingredients[0].quantity == 750.0
+    
+    with pytest.raises(ValueError):
+        recipe.scale(-67.0)
+    with pytest.raises(ValueError):
+        recipe.scale(0.0)
+
+def test_recipe_len():
+    recipe = Recipe("Пицца")
+    recipe.add_ingredient(Ingredient("Мука", 500.0, "г"))
+    recipe.add_ingredient(Ingredient("Соль", 1.0, "г"))
+    recipe.add_ingredient(Ingredient("Мука", 200.0, "г")) 
+    
+    assert len(recipe) == 2
